57395187a05d7cabef935612265d148d78ba4236
Ashish Rawat <ashish9342@gmail.com>
2019-09-02 00:39:25 +0530

    addTwoDigits function implemeted new solution

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -3,3 +3,11 @@ function addTwoDigits(n: any): number {
 }
 
 console.log(addTwoDigits(29));
+
+function addTwoDigits1(n: any): number {
+	const num = n.toString().split("");
+	console.log(num);
+	return num.reduce((a: string, b: string) => parseInt(a) + parseInt(b));
+}
+
+console.log(addTwoDigits1(29));
