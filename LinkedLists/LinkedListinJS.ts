1204d599c2488cbba941fce38f11ef9875d3b679
Ashish Rawat <ashish9342@gmail.com>
2019-12-31 17:19:53 +0530

    insert

---
+++ LinkedLists/LinkedListinJS.ts
@@ -0,0 +1,91 @@
+//  10 => 5 => 16
+
+// const myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class LinkedList {
+  head: any;
+  tail: any;
+  length: number;
+
+  constructor(value: any) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  public append(value: number) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
+    // console.log(this);
+
+    return this;
+  }
+
+  public prepend(value: number) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.length++;
+    return this;
+  }
+
+  public print() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+    return array;
+  }
+
+  public insert(index: number, value: number) {
+    if (index > this.length) {
+      return this.append(value);
+    }
+
+    const leader = this.traverseToIndex(index - 1);
+  }
+
+  private traverseToIndex(index: number) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return currentNode;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.print();
---
+++ LinkedLists/README.md
@@ -0,0 +1,27 @@
+# Linked List in Javascript
+
+## Why Linked List
+
+1. Loose struture
+2. Insert anything and anywhere
+3. traversal = > itteration
+4. scattered all over the memory
+5. itteration is slower than arrays
+
+## Big(O)
+
+1. prepend => o(1)
+2. append => o(1)
+3. lookup => o(n)
+4. insert => o(n) | worst case
+5. delete => o(n) | worst case
+
+### Pointer
+
+reference to memory to something else
+
+### Garbage collection
+
+delete
+
+https://visualgo.net/en/list
